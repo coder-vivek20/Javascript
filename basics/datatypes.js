14ea18787043dfc6a00c29ed5fbafec17651ab08
coder-vivek20 <chavanvivek681@gmail.com>
2024-07-26 22:54:45 +0530

    data types completed

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+ console.log("vivek")
+
+ const password="12345"
+ let city="pune"
+ var username="vivek chavan"
+ age=20
+
+city="pimpri chinchwad"
+username="vivek"
+age=21
+
+let ans=true
+
+if(ans){
+    console.log("you are true")
+}else{
+    console.log("you are wrong")
+}
+
