4f3a1f5eaa6ff48a647c7424fb6129423c241a84
coder-vivek20 <chavanvivek681@gmail.com>
2024-07-27 16:19:23 +0530

    complete datatype conversion part

---
+++ basics/ConversionOperation.js
@@ -0,0 +1,15 @@
+let score=55
+
+console.log(typeof(score))
+
+let conversionString=String(score)
+console.log(typeof conversionString)
+console.log(conversionString)
+
+let score1="55A";
+
+console.log(typeof score1)
+
+let convertscore=Number(score1)
+console.log(typeof convertscore)
+console.log(convertscore)
