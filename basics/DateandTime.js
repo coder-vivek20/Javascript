01a39280bcec742d44d83f521d52509ec71f78c5
coder-vivek20 <chavanvivek681@gmail.com>
2024-07-27 17:29:07 +0530

    adding date and time concepts and String concepts

---
+++ basics/DateandTime.js
@@ -0,0 +1,12 @@
+//today we are learning date and time
+
+//so to print current date we can use 
+
+const now = new Date()
+console.log(now);
+
+const newDate = Date(2024,6,12,12,45,55)
+console.log(newDate);
+
+const currentDate= new Date("2003-04-19")
+console.log(currentDate);
---
+++ basics/String.js
@@ -0,0 +1,11 @@
+//we are learning string
+//so firstly the value which we put in between this " " double quotes and in ' ' both single and double works.
+let name="vivek"
+let age=20
+let city="delhi"
+
+//for printing all values lets concat these strings 
+console.log(name+" "+age+" "+city)
+
+//we can print with this format also
+console.log(`my name is ${name} and my age is ${age} from ${city}`)
