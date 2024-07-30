b93e2707f228aa438fd5457ac04c961164e62391
coder-vivek20 <chavanvivek681@gmail.com>
2024-07-30 21:31:28 +0530

    array file is added

---
+++ basics/arrays.js
@@ -0,0 +1,12 @@
+//we are learning arrays
+
+const myArray = [1, "hello", true, null];
+
+
+console.log(myArray[1]);
+
+const newArray = new Array(3)
+
+newArray=[1,2,3]
+
+console.log(newArray[1]);
