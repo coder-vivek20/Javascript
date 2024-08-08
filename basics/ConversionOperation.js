6a052341e411672ea1d7e1fbc16b4eb4c36c8f0d
coder-vivek20 <chavanvivek681@gmail.com>
2024-08-08 12:04:13 +0530

    "Updated ConversionOperation.js and DateandTime.js files with additional code and comments."

--- basics/ConversionOperation.js
+++ basics/ConversionOperation.js
@@ -13,3 +13,7 @@ console.log(typeof score1)
 let convertscore=Number(score1)
 console.log(typeof convertscore)
 console.log(convertscore)
+
+//so first one will definately converts number into string but in this case 
+//let score1="55A"; suppose we have number and one character also. so it will convert to number but value  -
+// - will be NaN(Not a Number)
--- basics/DateandTime.js
+++ basics/DateandTime.js
@@ -1,12 +1,13 @@
 //today we are learning date and time
 
 //so to print current date we can use 
-
 const now = new Date()
 console.log(now);
 
+//to prints custom dates 
 const newDate = Date(2024,6,12,12,45,55)
 console.log(newDate);
 
+//to print dates in string
 const currentDate= new Date("2003-04-19")
 console.log(currentDate);
